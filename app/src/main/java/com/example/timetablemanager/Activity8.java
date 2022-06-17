1be67c034b89dd282c56f59e61caaa8c6052a32d
AshwinR27 <86276308+AshwinR27@users.noreply.github.com>
2022-06-17 21:06:28 +0530

    idk

--- app/src/main/java/com/example/timetablemanager/Activity8.java
+++ app/src/main/java/com/example/timetablemanager/Activity8.java
@@ -62,13 +62,6 @@ public class Activity8 extends AppCompatActivity {
         fri6 = (EditText) findViewById(R.id.Fri6);
         fri7 = (EditText) findViewById(R.id.Fri7);
 
-        sat1 = (EditText) findViewById(R.id.Sat1);
-        sat2 = (EditText) findViewById(R.id.Sat2);
-        sat3 = (EditText) findViewById(R.id.Sat3);
-        sat4 = (EditText) findViewById(R.id.Sat4);
-        sat5 = (EditText) findViewById(R.id.Sat5);
-        sat6 = (EditText) findViewById(R.id.Sat6);
-        sat7 = (EditText) findViewById(R.id.Sat7);
 
         abort = (Button) findViewById(R.id.abortbtn);
         save = (Button) findViewById(R.id.savebtn);
@@ -76,57 +69,57 @@ public class Activity8 extends AppCompatActivity {
         db = openOrCreateDatabase("TimeT", Context.MODE_PRIVATE,null);
         db.execSQL("CREATE TABLE IF NOT EXISTS TimeT(Section TEXT," +
                 "Day TEXT," +
+                "Teacher TEXT," +
                 "Subject TEXT," +
                 "primary key(Section,Day));");
-        try {
+
+
+
+//        Bundle bundle = getIntent().getBundleExtra("sect");
+//        String section = bundle.getString("sec");
+        String sec = "CSEA";
+        try{
             save.setOnClickListener(v -> {
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON1','" + mon1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON2','" + mon2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON3','" + mon3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON4','" + mon4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON5','" + mon5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON6','" + mon6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','MON7','" + mon7.getText() + "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON1','KKM','" + mon1.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON2','ANS','" + mon2.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON3','MDS','" + mon3.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON4','PRT','" + mon4.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON5','KKM','" + mon5.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON6','KKM','" + mon6.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','MON7','KKM','" + mon7.getText().toString()+ "');");
 
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE1','" + tue1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE2','" + tue2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE3','" + tue3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE4','" + tue4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE5','" + tue5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE6','" + tue6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','TUE7','" + tue7.getText() + "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE1','PRT','" + tue1.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE2','MDS','" + tue2.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE3','KKM','" + tue3.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE4','ANS','" + tue4.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE5','ANS','" + tue5.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE6','ANS','" + tue6.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','TUE7','ANS','" + tue7.getText().toString()+ "');");
 
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED1','" + wed1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED2','" + wed2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED3','" + wed3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED4','" + wed4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED5','" + wed5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED6','" + wed6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','WED7','" + wed7.getText() + "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED1','RKS','" + wed1.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED2','ASM','" + wed2.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED3','ASM','" + wed3.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED4','ASM','" + wed4.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED5','MDS','" + wed5.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED6','MDS','" + wed6.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','WED7','','" + wed7.getText().toString()+ "');");
 
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU1','" + thu1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU2','" + thu2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU3','" + thu3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU4','" + thu4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU5','" + thu5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU6','" + thu6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','THU7','" + thu7.getText() + "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU1','PRT','" + thu1.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU2','ANS','" + thu2.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU3','KKM','" + thu3.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU4','','" + thu4.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU5','','" + thu5.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU6','','" + thu6.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','THU7','','" + thu7.getText().toString()+ "');");
 
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI1','" + fri1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI2','" + fri2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI3','" + fri3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI4','" + fri4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI5','" + fri5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI6','" + fri6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','FRI7','" + fri7.getText() + "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI1','RKS','" + fri1.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI2','KKM','" + fri2.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI3','ANS','" + fri3.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI4','MDS','" + fri4.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI5','','" + fri5.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI6','','" + fri6.getText().toString()+ "');");
+                db.execSQL("INSERT INTO TimeT VALUES('"+sec+"','FRI7','','" + fri7.getText().toString()+ "');");
 
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT1','" + sat1.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT2','" + sat2.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT3','" + sat3.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT4','" + sat4.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT5','" + sat5.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT6','" + sat6.getText() + "');");
-                db.execSQL("INSERT INTO TimeT VALUES('CSEA','SAT7','" + sat7.getText() + "');");
 
                 Toast.makeText(getBaseContext(), "Time-Table Successfully Updated", Toast.LENGTH_LONG).show();
             });
