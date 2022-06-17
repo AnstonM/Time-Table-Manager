49ba87486cef5e45d26c3fb94cbb994537cfcb77
AnstonM <87513070+AnstonM@users.noreply.github.com>
2022-06-17 07:38:14 +0530

    Time table theme change

--- app/src/main/java/com/example/timetablemanager/Activity2.java
+++ app/src/main/java/com/example/timetablemanager/Activity2.java
@@ -13,7 +13,7 @@ public class Activity2 extends AppCompatActivity implements View.OnClickListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_1);
+        setContentView(R.layout.activity_2);
 
         ABtn = (Button) findViewById(R.id.btn1);
         ABtn.setOnClickListener(this);
